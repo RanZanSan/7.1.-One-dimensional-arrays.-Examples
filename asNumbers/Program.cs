0c92ec21ac488c4774121dce43289c091fb2a480
Andrey Rasskazov <ranzansan@gmail.com>
2021-07-27 15:52:47 +0300

    update

---
+++ asNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine()); // сколько чисел будут вводиться на следующей строке
+            string s = Console.ReadLine(); // сохранить всю введенную строку
+            string[] ss = s.Split(' '); // разделить введенную строку по пробелам и сохранить в массив строкового типа
+            int[] numbers = new int[n]; // заводим новый массив целого типа
+            for (int i = 0; i < n; i++) // так как у нас в массиве n чисел, то проходим по нему циклом
+            {
+                int number = Convert.ToInt32(ss[i]); // конвертируем в число
+                numbers[i] = number; // записываем в соответствующее место сконвертированное число
+            }
+        }
+    }
+}
---
+++ inputAndOutput/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inputAndOutput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine()); // сколько чисел будут вводиться на следующей строке
+            string s = Console.ReadLine(); // сохранить всю введенную строку
+            string[] ss = s.Split(' '); // разделить введенную строку по пробелам и сохранить в массив строкового типа
+        }
+    }
+}
